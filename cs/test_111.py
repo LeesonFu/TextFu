482da59f904cd28dc1b853dd6c29f2dec0b2dc6e
LeesonFu <iBWMi@icloud.com>
2021-12-14 19:19:23 +0800

    第二次

--- cs/test_111.py
+++ cs/test_111.py
@@ -2,32 +2,32 @@ from selenium import webdriver
 import time
 import pytest
 
-
-@pytest.mark.repeat(8)
-def test_one():
-    driver = webdriver.Edge()
-    driver.get('https://q.yiban.cn/app/index/appid/707223#block2')
-    time.sleep(10800)
-    driver.find_element('id', 'account-txt').send_keys('13826734251')
-    driver.find_element('id', 'password-txt').send_keys('8515365m2')
-    driver.find_element('id', 'login-btn').click()
-    driver.implicitly_wait(5)
-
-    driver.find_element('class name', 'morelink-img').click()
-    driver.implicitly_wait(5)
-
-    handle = driver.window_handles
-    driver.switch_to.window(handle[1])
-    driver.find_element('id', 'rk').click()
-    driver.implicitly_wait(5)
-
-    driver.find_element('id', 'an').click()
-    localtime = time.asctime(time.localtime(time.time()))
-    print('时间：', localtime)
-
-    time.sleep(50)
-    driver.quit()
-
-
-if __name__ == '__main__':
-    pytest.main(["-s", "test_111.py"])
+#
+# @pytest.mark.repeat(8)
+# def test_one():
+#     driver = webdriver.Edge()
+#     driver.get('https://q.yiban.cn/app/index/appid/707223#block2')
+#     time.sleep(10800)
+#     driver.find_element('id', 'account-txt').send_keys('13826734251')
+#     driver.find_element('id', 'password-txt').send_keys('8515365m2')
+#     driver.find_element('id', 'login-btn').click()
+#     driver.implicitly_wait(5)
+#
+#     driver.find_element('class name', 'morelink-img').click()
+#     driver.implicitly_wait(5)
+#
+#     handle = driver.window_handles
+#     driver.switch_to.window(handle[1])
+#     driver.find_element('id', 'rk').click()
+#     driver.implicitly_wait(5)
+#
+#     driver.find_element('id', 'an').click()
+#     localtime = time.asctime(time.localtime(time.time()))
+#     print('时间：', localtime)
+#
+#     time.sleep(50)
+#     driver.quit()
+#
+#
+# if __name__ == '__main__':
+#     pytest.main(["-s", "test_111.py"])
